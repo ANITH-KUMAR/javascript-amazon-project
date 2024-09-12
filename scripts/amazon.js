8b27dde077c65aac6ceee91cbe45020f02cd7a53
anith <anithkumarvadlamudi@gmail.com>
2024-09-12 23:42:11 +0530

    use modules to import cart,products using module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
-
-
+import {cart} from '../data/cart.js'
+import { products } from '../data/products.js';
 let productshtml = "";
 products.forEach((product) => {
   productshtml += `<div class="product-container">
@@ -21,7 +21,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ₹${(product.priceCents * 0.1*3).toFixed(2)}
+            ₹${(product.priceCents * 0.1 * 3).toFixed(2)}
           </div>
 
           <div class="product-quantity-container ">
@@ -38,15 +38,13 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
-
+          <div class="added js-modify-${product.id}">
+            <img id="img" src="images/icons/checkmark.png">Added
+          </div>
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart"
+          <button class="add-to-cart-button button-primary js-add-to-cart" 
           data-product-id="${product.id}">
             Add to Cart
           </button>
@@ -62,6 +60,20 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
        button.addEventListener('click',()=>{
         const productId=button.dataset.productId;
         let c=0;
+
+        const k = document.querySelector(`.js-modify-${productId}`);
+        
+        const intervalId=setInterval(()=>{
+              k.classList.add("added-remove");
+        },0);
+
+        setTimeout(()=>{
+          clearInterval(intervalId);
+           k.classList.remove("added-remove");
+
+        },3000);
+
+       
 
       let find = document.querySelector(`.js-quantity-selector-${productId}`);
      
